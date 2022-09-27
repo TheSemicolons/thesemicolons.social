f2956022c6e6a62b6c7071fe9d8ce5ae8e8e15f0
Jim Anderson <thesemicolons@protonmail.com>
2022-09-26 18:27:54 -0600

    Added link for treatstream and script to generate qr codes.

---
+++ generate_qrcode.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_live.svg https://thesemicolons.com
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_contribute.svg https://thesemicolons.social/contribute
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_discord.svg https://thesemicolons.social/discord
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_fanhouse.svg https://thesemicolons.social/fanhouse
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_food.svg https://thesemicolons.social/food
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_instagram.svg https://thesemicolons.social/instagram
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_patreon.svg https://thesemicolons.social/patreon
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_social.svg https://thesemicolons.social
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_steam.svg https://thesemicolons.social/steam
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_throne.svg https://thesemicolons.social/throne
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_twitch.svg https://thesemicolons.social/twitch
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_twitter.svg https://thesemicolons.social/twitter
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_website.svg https://thesemicolons.social/website
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_youtube.svg https://thesemicolons.social/youtube
