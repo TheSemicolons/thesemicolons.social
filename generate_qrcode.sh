6dae4cf2e869632a73cf610fb740365b2219d54e
Jim Anderson <thesemicolons@protonmail.com>
2022-09-29 17:55:14 -0600

    Changed contribute to support.

--- generate_qrcode.sh
+++ generate_qrcode.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_live.svg https://thesemicolons.com
-qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_contribute.svg https://thesemicolons.social/contribute
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_discord.svg https://thesemicolons.social/discord
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_fanhouse.svg https://thesemicolons.social/fanhouse
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_food.svg https://thesemicolons.social/food
@@ -8,6 +7,7 @@ qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_instagram.svg https://thesemicolons.s
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_patreon.svg https://thesemicolons.social/patreon
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_social.svg https://thesemicolons.social
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_steam.svg https://thesemicolons.social/steam
+qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_support.svg https://thesemicolons.social/support
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_throne.svg https://thesemicolons.social/throne
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_twitch.svg https://thesemicolons.social/twitch
 qrencode -s 5 -l H -m 5 -d 96 -t SVG -o qr_twitter.svg https://thesemicolons.social/twitter
